c45133d04e16d735b7bd0ff044c732f41a1ecdbe
nmkataoka <nmkataoka@gmail.com>
2018-11-18 13:47:14 -0500

    add idp login/register helper functions

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -90,7 +90,26 @@ export function registerUser (name, email, password, password_confirmation) {
     })
     .catch(error => {
       return error_handle(error)
+    });
+}
+
+// If successful, will return access_token
+export function registerUserIdp(data) {
+  return axios
+    .post('oauth/token', {
+      grant_type: 'idp',
+      idp: data['idp'],
+      client_id: 1,
+      client_secret:'',
+      idp_token: data['idp_token'],
+      register: true
     })
+    .then(response => {
+      return respond(response.status, response.data);
+    })
+    .catch(error => {
+      return error_handle(error);
+    });
 }
 
 export function loginUser (email, password) {
@@ -142,23 +161,23 @@ export function loginUser (email, password) {
     })
 }
 
-export function loginUserIdp (email, idp, accessToken) {
+// If successful, will return an access_token
+export function loginUserIdp (data) {
   sessionStorage.clear()
 
   // Login
-
   return axios
     .post('oauth/token', {
       client_id: 1,
+      client_secret: "",
       grant_type: 'idp',
-      idp: idp,
-      idpToken: accessToken,
-      register: false
+      idp: data['idp'],
+      idp_token: data['token']
     })
     .then(response => {})
     .catch(error => {
-      return error_handle(error)
-    })
+      return error_handle(error);
+    });
 }
 
 export function logoutCurrentUser () {
